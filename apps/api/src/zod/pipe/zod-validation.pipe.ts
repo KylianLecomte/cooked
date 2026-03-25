3ef079342d274e682d4cdcc99050bd1fed308d4f
Kylian <kylian.lec@gmail.com>
2026-03-25 19:48:38 +0100

    feat(api): replace zod schema with zod type

--- apps/api/src/zod/pipe/zod-validation.pipe.ts
+++ apps/api/src/zod/pipe/zod-validation.pipe.ts
@@ -1,10 +1,10 @@
 import { Injectable, PipeTransform } from "@nestjs/common";
-import { ZodSchema } from "zod";
+import { ZodType } from "zod";
 import { zodSafeParse } from "../util/zod.util";
 
 @Injectable()
 export class ZodValidationPipe implements PipeTransform {
-  constructor(private readonly schema: ZodSchema) {}
+  constructor(private readonly schema: ZodType) {}
 
   transform(value: unknown) {
     return zodSafeParse(this.schema, value);
